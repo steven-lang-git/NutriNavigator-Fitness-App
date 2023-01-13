31a5ba0d42b4b435cef093ac633ee515353cea3e
Steven Lang <scubaastev@gmail.com>
2023-01-12 20:28:02 -0800

    updated how the data loads onto the table for the page

--- react-frontend/src/Components/Tracking.js
+++ react-frontend/src/Components/Tracking.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import BottomMenu from './BottomMenu'
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
@@ -6,48 +6,74 @@ import "../styles/Home.css"
 import Header from './header';
 import { CssBaseline } from '@material-ui/core';
 import EnhancedTable from './EnhancedTable'
-import makeData from '../makeData'
+// import makeData from '../makeData'
+import { gql, useLazyQuery, useQuery } from "@apollo/client";
+import AddMeal from "./AddMeal";
+import createBreakpoints from '@material-ui/core/styles/createBreakpoints';
 
-
+//try to run a mutation in here to see if that works
 function Tracking() {
     const columns = React.useMemo(
         () => [
             {
-                Header:'Food Name',
-                accessor:'foodname',
+                Header: 'Food Name',
+                accessor: 'foodname',
             },
             {
-                Header:'Calories',
-                accessor:'calories',
+                Header: 'Calories',
+                accessor: 'calories',
             },
             {
-                Header:'Fat',
-                accessor:'fat',
+                Header: 'Fat',
+                accessor: 'fat',
             },
             {
-                Header:'Carbs',
-                accessor:'carbs',
+                Header: 'Carbs',
+                accessor: 'carbs',
             },
             {
-                Header:'Protein',
-                accessor:'protein',
+                Header: 'Protein',
+                accessor: 'protein',
             },
 
         ],
         []
     )
+    const GET_MEALS = gql`
+    {
+        meals{
+            id
+            foodname
+            calories
+            fat
+            carbs
+            protein
+        }
+    }
+    `;
 
-    // const [data,setData] = React.useState(React.useMemo(()=>makeData(20),[]))
-    // useMemo returns a memoized memo - optimization technique by storing results of expensive function calls and returning the cache result
-    const [data,setData] = React.useState([])
+
+
+    const list_columns = ['foodname', 'calories', 'fat', 'carbs', 'protein']
+
     const [skipPageReset, setSkipPageReset] = React.useState(false)
-
-
-    const totalCalsValue = ((data.reduce((a, v) => a = a + parseInt(v.calories), 0)));
-    const totalFatValue = ((data.reduce((a, v) => a = a + parseInt(v.fat), 0)));
-    const totalCarbsValue = ((data.reduce((a, v) => a = a + parseInt(v.carbs), 0)));
-    const totalProteinValue = ((data.reduce((a, v) => a = a + parseInt(v.protein), 0)));
-
+    const { loading, error, data, refetch } = useQuery(GET_MEALS)
+    const [totalCalsValue, setTotalCalsValue] = React.useState(0)
+    const [totalFatValue, setTotalFatValue] = React.useState(0)
+    const [totalCarbsValue, setTotalCarbsValue] = React.useState(0)
+    const [totalProteinValue, setTotalProteinValue] = React.useState(0)
+    const [info, setInfo] = React.useState([])
+    useEffect(() => {
+        if (!loading) {
+            setInfo(data.meals)
+            setTotalCalsValue((data.meals.reduce((a, v) => a = a + parseInt(v.calories), 0)) ?? 0)
+            setTotalFatValue((data.meals.reduce((a, v) => a = a + parseInt(v.fat), 0)) ?? 0)
+            setTotalCarbsValue((data.meals.reduce((a, v) => a = a + parseInt(v.carbs), 0)) ?? 0)
+            setTotalProteinValue((data.meals.reduce((a, v) => a = a + parseInt(v.protein), 0)) ?? 0)
+            console.log("Page data", data.meals);
+            refetch()
+        }
+    }, [loading, data])
 
     var totalMacros = {
         'totalCals': totalCalsValue,
@@ -72,54 +98,58 @@ function Tracking() {
     }
     // stop table from resetting page index when you update data
     //when cell renderer calls updateData, will use rowIndex, columnID and new value to update original data
-    const updateData = (rowIndex, columnId, value)=>{
+    const updateData = (rowIndex, columnId, value) => {
         setSkipPageReset(true)
-        setData( old=>
-            old.map((row,index)=>{
-                if (index===rowIndex){
-                    return{
+        setInfo(old =>
+            old.map((row, index) => {
+                if (index === rowIndex) {
+                    return {
                         ...old[row],
-                        [columnId]:value,
+                        [columnId]: value,
                     }
                 }
                 return row
             })
-            )
+        )
     }
 
 
     return (
         <div className="right">
-                               <Header />
+            <Header />
 
             <Grid container spacing={3}>
                 {/*Create items with different breakpoints */}
                 {/*For example,This item will be 12 units wide on extra small screens */}
                 <Grid item xs={12}>
-                    <h1>Tracking</h1>
-
+                    <h1>Meals</h1>
                     <Paper style={{ display: "flex", justifyContent: "center", alignItems: "center", textAlign: "center", height: 100, margin: 10 }}>
-
                         <h4>
-                            Track everything from your weight and average heart rates.
+                            What we've eaten so far...
                         </h4>
                     </Paper>
                 </Grid>
                 <Grid item xs={12} sm={12}>
                     <Paper style={{ flexDirection: "column", justifyContent: "center", alignItems: "center", margin: 10, display: "flex", padding: "25px 50px 75px 50px" }}>
-                    <div>
-                        <CssBaseline/>
-                        <EnhancedTable
-                            columns={columns}
-                            data={data}
-                            setData={setData}
-                            updateData={updateData}
-                            skipPageReset={skipPageReset}
+                        <div>
+                            <CssBaseline />
+                            {/*make sure it is rendered before loading the data */}
+                            {/* if equates to true the expression after && will render */}
+                            {loading}
+                            {error && <p>Something went wrong...</p>}
 
-                        />
-                    </div>
+                            {
+                                <EnhancedTable
+                                    columns={columns}
+                                    data={info}
+                                    setData={setInfo}
+                                    updateData={updateData}
+                                    skipPageReset={skipPageReset}
+
+                                />}
+                        </div>
                     </Paper>
-                    </Grid>
+                </Grid>
                 {/*This item will be 12 units on extra small screens */}
                 {/*But will be 6 units on small screens */}
                 <Grid item xs={6} sm={3}>
@@ -154,24 +184,8 @@ function Tracking() {
                         <span style={{ color: 'hsl(203, 89%, 64%)' }}><h1>{remMacros.totalProtein}&nbsp;(g)</h1></span>
                     </Paper>
                 </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Paper style={{ height: 100, margin: 10 }}>xs=12 sm=6</Paper>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Paper style={{ height: 100, margin: 10 }}>xs=12 sm=6</Paper>
-                </Grid>
-                <Grid item xs={6} sm={3}>
-                    <Paper style={{ height: 100, margin: 10 }}>xs=6 sm=3</Paper>
-                </Grid>
-                <Grid item xs={6} sm={3}>
-                    <Paper style={{ height: 100, margin: 10 }}>xs=6 sm=3</Paper>
-                </Grid>
-                <Grid item xs={6} sm={3}>
-                    <Paper style={{ height: 100, margin: 10 }}>xs=6 sm=3</Paper>
-                </Grid>
-                <Grid item xs={6} sm={3}>
-                    <Paper style={{ height: 100, margin: 10 }}>xs=6 sm=3</Paper>
-                </Grid>
+
+
             </Grid>
 
             <BottomMenu />
